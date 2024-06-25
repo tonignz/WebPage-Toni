6ad1a9def383b88a450f04d625e1df27433a7ccc
ToniKelao <80598496+ToniKelao@users.noreply.github.com>
2024-06-25 18:49:17 +0200

    Modify the ShowContent function to highlight the selected content in the sidebar menu. Also add comments in the scripts

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function showContent(contentId) {
+function showContent(contentId,clickedElement) {
     // Ocultar todos los elementos de contenido
     var contents = document.querySelectorAll('.content');
     contents.forEach(function(content) {
@@ -8,8 +8,22 @@ function showContent(contentId) {
     // Mostrar el contenido relacionado con el elemento clicado
     var contentToShow = document.getElementById(contentId);
     contentToShow.style.display = 'block';
+
+    // Seleccionar todos los enlaces del menú y poner el peso de la fuente a "normal"
+    var links = document.querySelectorAll(".menu-link");
+    links.forEach(function(link) {
+        link.style.fontWeight = "normal";
+    });
+    
+    // Poner el peso de la fuente del enlace clicado a "bold"
+    clickedElement.style.fontWeight = "bold";
+
 }
 
+// Ejecutar cuando la página se haya cargado completamente
 window.onload = function() {
-    showContent('AboutMe');
+    // Seleccionar el primer enlace del menú por defecto
+    var defaultClicked = document.querySelector('ul li a');
+    // Mostrar el contenido por defecto y resaltar el primer enlace del menú
+    showContent('AboutMe',defaultClicked);
 };
